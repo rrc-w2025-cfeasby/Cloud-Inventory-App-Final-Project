f3cd9d4a49ce860f3e405ebd69d26f3191a69571
Chris Feasby <cfeasby@academic.rrc.ca>
2026-03-23 11:29:15 -0500

    Initial Project structure with workflows and lambda functions

---
+++ lambda/get_all_inventory_items/lambda_function.py
@@ -0,0 +1,23 @@
+import boto3
+import json
+
+def lambda_handler(event, context):
+    dynamo = boto3.resource('dynamodb')
+    table = dynamo.Table('Inventory')
+
+    try:
+        response = table.scan()
+        items = response.get('Items', [])
+
+        clean_items = json.loads(json.dumps(items, default=str))
+
+        return {
+            'statusCode': 200,
+            'body': json.dumps(clean_items)
+        }
+
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps(str(e))
+        }
